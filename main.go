c57f223eb59b7756a1e2f74e9e5629d1771f86a9
Tejas Nikhar <tejastn10@gmail.com>
2025-01-15 06:02:05 +0530

    feat: move repo commands to pkg folder

--- main.go
+++ main.go
@@ -1,58 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"path/filepath"
+	"github.com/tejastn10/quill/cmd"
 )
-
-func createQuillRepository(path string) error {
-	// Defining the Quill directory structure
-	directories := []string{
-		filepath.Join(path, ".quill"),
-		filepath.Join(path, ".quill", "objects"),
-		filepath.Join(path, ".quill", "refs"),
-		filepath.Join(path, ".quill", "config"),
-	}
-
-	// Creating directories
-	for _, dir := range directories {
-		err := os.MkdirAll(dir, 0750)
-		if err != nil {
-			return fmt.Errorf("failed to create the directory %s: %w", dir, err)
-		}
-	}
-
-	return nil
-}
-
-func checkQuillExists(path string) bool {
-	quillPath := filepath.Join(path, ".quill")
-	_, err := os.Stat(quillPath)
-	return !os.IsNotExist(err)
-}
 
 func main() {
 	// Get the current working Directory
-	workingDir, err := os.Getwd()
-	if err != nil {
-		fmt.Println("Error: Unable to get the current working directory:", err)
-		return
-	}
-
-	// Check if Quill is already initialized
-	quillExists := checkQuillExists(workingDir)
-	if quillExists {
-		fmt.Println("Error: A .quill repository already exists in this directory.")
-		return
-	}
-
-	// Create .quill repository structure
-	err = createQuillRepository(workingDir)
-	if err != nil {
-		fmt.Println("Error: Failed to initialize repository:", err)
-		return
-	}
-
-	fmt.Println("Initialized empty Quill repository in", filepath.Join(workingDir, ".quill"))
+	cmd.Execute()
 }
---
+++ pkg/repo/init.go
@@ -0,0 +1,33 @@
+package repo
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func CreateQuillRepository(path string) error {
+	// Defining the Quill directory structure
+	directories := []string{
+		filepath.Join(path, ".quill"),
+		filepath.Join(path, ".quill", "objects"),
+		filepath.Join(path, ".quill", "refs"),
+		filepath.Join(path, ".quill", "config"),
+	}
+
+	// Creating directories
+	for _, dir := range directories {
+		err := os.MkdirAll(dir, 0750)
+		if err != nil {
+			return fmt.Errorf("failed to create the directory %s: %w", dir, err)
+		}
+	}
+
+	return nil
+}
+
+func CheckQuillExists(path string) bool {
+	quillPath := filepath.Join(path, ".quill")
+	_, err := os.Stat(quillPath)
+	return !os.IsNotExist(err)
+}
