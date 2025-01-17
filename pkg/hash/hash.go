2b5055d430058f63a663cb8513c29442058460ed
Tejas Nikhar <tejastn10@gmail.com>
2025-01-17 05:56:19 +0530

    feat: remove SHA1 as it is not safe anymore

--- pkg/hash/hash.go
+++ pkg/hash/hash.go
@@ -1,20 +1,11 @@
 package hash
 
 import (
-	"crypto/sha1"
 	"crypto/sha256"
 	"encoding/hex"
 
 	"golang.org/x/crypto/blake2b"
 )
-
-// Compute a SHA-1 hash for the given data.
-func ComputeSHA1(data []byte) string {
-	hasher := sha1.New()
-	hasher.Write(data)
-
-	return hex.EncodeToString(hasher.Sum(nil))
-}
 
 // Compute a SHA-256 hash for the given data.
 func ComputeSHA256(data []byte) string {
