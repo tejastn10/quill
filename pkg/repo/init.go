abdd8a4128a7e68c2dce1995b0a9f16c0feb122e
Tejas Nikhar <tejastn10@gmail.com>
2025-02-06 13:13:57 +0530

    feat: add CreateUserConfig function to create user configuration file

--- pkg/repo/init.go
+++ pkg/repo/init.go
@@ -57,3 +57,32 @@ func FindRepoRoot() (string, error) {
 
 	return "", errors.New("not a quill repository (or any of the parent directories): .quill")
 }
+
+func CreateUserConfig(name string, email string) error {
+	// Get the current working Directory
+	workingDir, err := os.Getwd()
+	if err != nil {
+		return fmt.Errorf("unable to get the current working directory: %w", err)
+	}
+
+	// Create user config file
+	userConfigDir := filepath.Join(workingDir, ".quill", "config")
+	err = os.MkdirAll(userConfigDir, os.ModePerm)
+	if err != nil {
+		return fmt.Errorf("failed to create config directory: %w", err)
+	}
+
+	userConfigFile := filepath.Join(userConfigDir, "user")
+	file, err := os.Create(userConfigFile)
+	if err != nil {
+		return fmt.Errorf("failed to create user config file: %w", err)
+	}
+	defer file.Close()
+
+	_, err = file.WriteString(fmt.Sprintf("name=%s\nemail=%s\n", name, email))
+	if err != nil {
+		return fmt.Errorf("failed to write user config file: %w", err)
+	}
+
+	return nil
+}
