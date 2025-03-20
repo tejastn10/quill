4443a471ecd10a9d24225a9f8ed3a50dc1606837
Tejas Nikhar <tejastn10@gmail.com>
2025-03-20 08:30:00 +0530

    feat: add ReadObject and WriteTree functions for object retrieval and tree serialization

--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
+
+	"github.com/tejastn10/quill/pkg/hash"
 )
 
 // Writing a file's contents as a blob in the .quill/objects directory.
@@ -37,4 +40,77 @@ func ObjectExists(repoPath string, hash string) bool {
 	objectPath := filepath.Join(repoPath, ".quill", "objects", hash[:2], hash[2:])
 	_, err := os.Stat(objectPath)
 	return err == nil
+}
+
+func ReadObject(repoPath, hash string) ([]byte, error) {
+	objectPath := filepath.Join(repoPath, ".quill", "objects", hash[:2], hash[2:])
+	data, err := os.ReadFile(objectPath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read object: %w", err)
+	}
+	return data, nil
+}
+
+func WriteTree(repoPath string) (string, error) {
+	var entries []string
+
+	workDir := filepath.Join(repoPath, ".quill", "staging")
+	err := filepath.Walk(workDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// Skip the root directory itself
+		if path == workDir {
+			return nil
+		}
+
+		// Compute relative path
+		relPath, _ := filepath.Rel(workDir, path)
+
+		// Read file content
+		var objectHash string
+		if info.IsDir() {
+			// If it's a directory, recursively generate its tree object
+			objectHash, err = WriteTree(repoPath)
+			if err != nil {
+				return err
+			}
+		} else {
+			// Read file contents and hash it
+			data, err := os.ReadFile(path)
+			if err != nil {
+				return err
+			}
+			objectHash = hash.ComputeSHA256(data)
+
+			// Store as a blob
+			err = CreateObject(repoPath, objectHash, data)
+			if err != nil {
+				return err
+			}
+		}
+
+		// Format: "<hash> <filetype> <filename>"
+		entry := fmt.Sprintf("%s %s %s", objectHash, "blob", relPath)
+		entries = append(entries, entry)
+
+		return nil
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	// Serialize tree contents
+	treeData := strings.Join(entries, "\n")
+	treeHash := hash.ComputeSHA256([]byte(treeData))
+
+	// Store tree object
+	err = CreateObject(repoPath, treeHash, []byte(treeData))
+	if err != nil {
+		return "", err
+	}
+
+	return treeHash, nil
 }
